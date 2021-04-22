461cc8b60452bc9860a67d0e52bf1238585aed42
Rodrigo Carrasco <rodrigo.bulboa@gmail.com>
2021-04-21 23:22:08 -0400

    Segundo Respaldo

--- menucafeteria/src/App.vue
+++ menucafeteria/src/App.vue
@@ -1,27 +1,19 @@
 <template>
   <div id="app">
     <b-container>
-      <div id="nav">
-        <!-- <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> -->
-      </div>
       <b-navbar toggleable="sm" type="light" variant="light">
         <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
-
-        <b-navbar-brand>Rigo Café</b-navbar-brand>
-
+        <b-navbar-brand>Al Rigo café</b-navbar-brand>
         <b-collapse id="nav-text-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/">Inicio</b-nav-item>
             <b-nav-item to="/menu">Menú</b-nav-item>
-            <b-nav-item to="/about">About</b-nav-item>
-            <b-nav-text>Navbar text</b-nav-text>
+            <b-nav-item to="/about">A cerca de</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
-
+      <br />
       <router-view />
-      <br>
       <p>Hecho por RC (C)</p>
     </b-container>
   </div>
--- menucafeteria/src/views/Menu.vue
+++ menucafeteria/src/views/Menu.vue
@@ -1,20 +1,32 @@
-<template>
+  <template>
   <div>
-    <b-card no-body header="Menú Cafetería Rigo">
+    <b-card no-body header="Menu Al Rigo Café">
       <b-list-group flush>
-        <b-list-group-item v-for="producto of productos" :key="producto.id">
-          {{ producto.nombre }} - {{ producto.precio }} - {{ producto.descripcion }} - {{ producto.foto }}
+        <b-list-group-item v-for="producto of productos" :key="producto.id"
+          >{{ producto.nombre }} - {{ producto.precio }}
+          <b-button-group class="float-right">
+            <b-button
+              class="mr-1"
+              variant="warning"
+              @click="modificar(producto.id)"
+              ><b-icon icon="pen-fill"></b-icon
+            ></b-button>
+            <b-button variant="danger" @click="eliminar(producto.id)"
+              ><b-icon icon="trash2-fill"></b-icon
+            ></b-button>
+          </b-button-group>
         </b-list-group-item>
       </b-list-group>
     </b-card>
+    <hr />
     <b-form @submit.prevent>
       <label>Nombre:</label>
       <b-form-input
         type="text"
-        placeholder="Descripción del producto"
+        placeholder="Nombre del producto"
         v-model="nombre"
       ></b-form-input>
-      <b-form-text>Nombre del Producto</b-form-text>
+      <b-form-text>Nombre del producto</b-form-text>
       <label>Descripción:</label>
       <b-form-textarea
         row="3"
@@ -23,31 +35,29 @@
         v-model="descripcion"
       ></b-form-textarea>
       <b-form-text>Descripción larga del producto</b-form-text>
-      <label>precio:</label>
-      <b-form-textarea
-        row="3"
-        max-rows="8"
+      <label>Precio:</label>
+      <b-form-input
+        type="number"
         placeholder="Precio del producto"
-        v-model="precio"
-      ></b-form-textarea>
+        v-model.number="precio"
+      ></b-form-input>
       <b-form-text>Precio del producto</b-form-text>
       <label>Foto:</label>
-      <b-form-textarea
-        row="3"
-        max-rows="8"
+      <b-form-input
+        type="url"
         placeholder="Foto del producto"
         v-model="foto"
-      ></b-form-textarea>
+      ></b-form-input>
       <b-form-text>Foto del producto</b-form-text>
-      <br>
-      <b-button @click="grabar()">Grabar</b-button> <!-- esto es un methods (metodo) -->
+      <b-button variant="primary" @click="grabar()"
+        ><b-icon icon="cloud-arrow-up-fill"></b-icon
+      ></b-button>
     </b-form>
   </div>
 </template>
 
 <script>
 import { db } from "@/db.js";
-
 export default {
   name: "Menu",
   data() {
@@ -55,18 +65,47 @@ export default {
       productos: [],
       nombre: "",
       precio: 0,
-      descripcio: "",
+      descripcion: "",
       foto: "",
+      id: "",
     };
   },
   methods: {
+    eliminar(id) {
+      db.collection("productos").doc(id).delete();
+    },
+    modificar(id) {
+      db.collection("productos")
+        .doc(id)
+        .get()
+        .then((snapshot) => {
+          if (snapshot.exists) {
+            let data = snapshot.data();
+            this.nombre = data.nombre;
+            this.precio = data.precio;
+            this.descripcion = data.descripcion;
+            this.foto = data.foto;
+            this.id = id;
+          }
+        });
+    },
     grabar() {
-      db.collection("productos").add({
-        nombre: this.nombre,
-        descripcion: this.descripcion,
-        precio: this.precio,
-        foto: this.foto,
-      });
+      if (this.id) {
+        db.collection("productos").doc(this.id).set({
+          nombre: this.nombre,
+          precio: this.precio,
+          descripcion: this.descripcion,
+          foto: this.foto,
+        });
+        this.id = "";
+      } else {
+        db.collection("productos").add({
+          nombre: this.nombre,
+          descripcion: this.descripcion,
+          precio: this.precio,
+          foto: this.foto,
+        });
+      }
       this.nombre = "";
       this.descripcion = "";
       this.precio = 0;
